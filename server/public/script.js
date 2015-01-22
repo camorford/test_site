d78a35c06e479be090bcb478ba8f566dcd7e2a03
Celeste <camorford@comcast.net>
2015-01-21 23:31:03 -0500

    testing git

---
+++ server/public/script.js
@@ -0,0 +1,8 @@
+jQuery(function ($) {
+    $('#accordion li').hover(function () {
+        $(this).find('ul').stop(true, true).slideDown()
+    }, function () {
+        $(this).find('ul').stop(true, true).slideUp()
+    }).find('ul').hide()
+
+})
